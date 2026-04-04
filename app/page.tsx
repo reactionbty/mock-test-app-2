1e8b5d2c38f63ca28bf435854f9022449d9fefbd
reactionbty <98457362+reactionbty@users.noreply.github.com>
2026-04-04 22:09:02 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { QuizContainer } from "@/components/quiz/quiz-container"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <QuizContainer />
-    </main>
-  )
-}
